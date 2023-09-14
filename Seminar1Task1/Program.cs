95d7ea8a4826ba2eaace2fb8053019c3be0c5d8a
Sandulyak_Svetlana <sve-sandulyak@yandex.ru>
2023-09-14 23:34:18 +0300

    Коммит .gitignore

---
+++ Seminar1Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите два числа");
+int numA = int.Parse(Console.ReadLine()??"0");
+int numB = int.Parse(Console.ReadLine()??"0");
+
+if(numB*numB == numA)
+{
+   System.Console.WriteLine("Первое число квадрат второго");
+}
+else
+{
+   System.Console.WriteLine("Первое число не квадрат второго");
+}
---
+++ Seminar1Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿int numN = int.Parse(Console.ReadLine()??"0");
+int evNumN = (-1)*numN;
+
+while (evNumN < numN)
+{
+    Console.Write(evNumN + ",");
+    evNumN++;
+}
+    Console.WriteLine(numN);
