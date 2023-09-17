3cec6c9fa636931ac24b376828645f719108c09a
Sandulyak_Svetlana <sve-sandulyak@yandex.ru>
2023-09-18 00:24:05 +0300

    solution from Seminar1Task7

---
+++ Seminar1Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число:");
+//Прием данных
+int N = Convert.ToInt32(Console.ReadLine());
+
+//Создание счетчика
+int count = 0;
+
+//Проверка на четность
+if (N % 2 != 0)
+{
+    //Делаем число четным
+    N = N - 1;
+}
+
+while (count < N)
+{
+    //Увеличение и вывод счетчика
+    count = count + 2;
+    Console.Write(count + " ");
+}
