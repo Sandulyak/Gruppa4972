2ca0e00176b9a9fbd149724d0ba95c5697d2fd4a
Sandulyak_Svetlana <sve-sandulyak@yandex.ru>
2023-09-18 00:40:41 +0300

    solution from Seminar1Task4

--- Seminar1Task4/Program.cs
+++ Seminar1Task4/Program.cs
@@ -1,32 +1,26 @@
-﻿Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-2 3 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22
-*/
-
-Console.WriteLine("Введите первое число:");
+﻿Console.WriteLine("Введите первое число:");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
+Console.WriteLine("Введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число:");
+Console.WriteLine("Введите третье число: ");
 int thirdNumber = Convert.ToInt32(Console.ReadLine());
 if(firstNumber > secondNumber)
 {
     if (firstNumber > thirdNumber)
     {
-        Console.WriteLine("Максимальное число:" + firstNumber);
+        Console.WriteLine("Максимальное число: " + firstNumber);
     }
     else
     {
-        Console.WriteLine("Максимальное число:" + thirdNumber);
+        Console.WriteLine("Максимальное число: " + thirdNumber);
     }
 }
 
 else if (secondNumber > thirdNumber)
 {
-    Console.WriteLine("Максимальное число:" + secondNumber);
+    Console.WriteLine("Максимальное число: " + secondNumber);
 }
 else
 {
-    Console.WriteLine("Максимальное число:" + thirdNumber);
+    Console.WriteLine("Максимальное число: " + thirdNumber);
 }
