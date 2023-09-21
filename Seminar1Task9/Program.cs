15f7d2879bf327fd88895ac8c3149c4216f88bbe
Sandulyak_Svetlana <sve-sandulyak@yandex.ru>
2023-09-21 13:18:03 +0300

    Solution from seminar2Task 8,11,12,14,16

---
+++ Seminar1Task9/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Способ 1");
+
+System.Random numberGenerator = new System.Random();
+int number = numberGenerator.Next(10,100);
+Console.WriteLine(number);
+
+
+int firstDigit = number/10;
+
+int secondDigit = number%10;
+
+if (firstDigit>secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+
+else
+{
+    Console.WriteLine(secondDigit);
+}
+
+Console.WriteLine("Способ 2");
+
+System.Random numberGenerator1 = new System.Random();
+int number1 = numberGenerator1.Next(10,100);
+Console.WriteLine(number1);
+
+Console.WriteLine((number/10>number%10)?(int)(number/10):(int)(number%10));
